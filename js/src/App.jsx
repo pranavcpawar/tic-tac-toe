9102f7f32a35b3152675ce9e7a344068ddcd8a2b
pranavcpawar <pranavcpawar@outlook.com>
2024-01-20 23:59:17 +0530

    add app

--- js/src/App.jsx
+++ js/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import Board from './components/Board'
 
 function App() {
 
+  const [squares, setSquares] = useState([0,1,2,3,4,5,6,7,8]);
+
+
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+      <div className='flex flex-col place-content-center gap-[40px] mx-auto'>
+        <h1 className='text-5xl font-semibold p-4'>TIC TAC TOE</h1>
+        <Board squares={squares} />
+        <button className="bg-dark_pink rounded-[8px] text-black text-lg font-semibold p-2 border-none">RESET</button>
       </div>
-    </>
   )
 }
 
